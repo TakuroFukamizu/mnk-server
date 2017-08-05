4d9b35bfe6da7cc070b3d02f0a6327c59707d5b7
t-fukamizu <fukamizu@neoscorp.jp>
2017-08-05 11:46:29 +0900

    MQTT向先変更

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -1,7 +1,7 @@
 export namespace Config {
     export const HTTP_PORT = 8080;
-    export const MQ_URL = 'mqtt://192.168.179.3';
-    // export const MQ_URL = 'mqtt://localhost';
+    // export const MQ_URL = 'mqtt://192.168.179.3';
+    export const MQ_URL = 'mqtt://localhost';
     export const MQ_USER = 'admin';
     export const MQ_PASS = 'raspberry';
     export const DEFAULT_DOCID = "1PjZ4KgqHhjmDMSmyA3-fDo8b1h48SWVO27pljt4TtlY";
